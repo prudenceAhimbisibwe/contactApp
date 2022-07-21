8ea0f74bff77217a0e0460ef825aff84c21c1460
PudenceAhimbisibwe <prudeahimbisibwe@gmail.com>
2022-07-21 19:07:39 +0300

    displaying images

--- app/src/main/java/com/prudence/contactapplication/ContactRvAdapter.kt
+++ app/src/main/java/com/prudence/contactapplication/ContactRvAdapter.kt
@@ -7,30 +7,32 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.prudence.contactapplication.databinding.ActivityMainBinding
+import com.prudence.contactapplication.databinding.ContactListItemBinding
+import com.squareup.picasso.Picasso
 
 class ContactRvAdapter (var contactList:List<Contact>):RecyclerView.Adapter<ContactViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-       var itemView=LayoutInflater.from(parent.context).inflate(R.layout.contact_list_item,parent,false)
-        return  ContactViewHolder(itemView)
+        var binding=ContactListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return  ContactViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
        var currntContact=contactList.get(position)
-        holder.tvName.text=currntContact.name
-        holder.tvphone.text=currntContact.phoneNumber
-        holder.tvemail.text=currntContact.email
-        holder.tvaddress.text=currntContact.address
+        holder.binding.tvName.text=currntContact.name
+        holder.binding.tvphone.text=currntContact.phoneNumber
+        holder.binding.tvemail.text=currntContact.email
+        holder.binding.tvaddress.text=currntContact.address
+        Picasso.get()
+            .load(currntContact.Image)
+            .resize(300,300)
+            .centerCrop()
+            .placeholder(R.drawable.ic_baseline_person_add_24)
+            .into(holder.binding.ivcontact)
     }
 
     override fun getItemCount(): Int {
         return contactList.size
     }
 }
-class ContactViewHolder(itemView: View):
-        RecyclerView.ViewHolder(itemView){
-            var tvName=itemView.findViewById<TextView>(R.id.tvName)
-            var tvphone=itemView.findViewById<TextView>(R.id.tvphone)
-            var tvemail=itemView.findViewById<TextView>(R.id.tvemail)
-            var tvaddress=itemView.findViewById<TextView>(R.id.tvaddress)
-            var ivcontact=itemView.findViewById<ImageView>(R.id.ivcontact)
-        }
+class ContactViewHolder(val binding: ContactListItemBinding): RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/prudence/contactapplication/MainActivity.kt
+++ app/src/main/java/com/prudence/contactapplication/MainActivity.kt
@@ -15,11 +15,11 @@ class MainActivity : AppCompatActivity() {
         displayContact()
     }
     fun displayContact(){
-        var contact=Contact("Prudence","prudeahimbisibwe@gmail.com","0784027930","uganda","")
-        var contact1=Contact("dallen","prudeahimbisibwe@gmail.com","0784027930","uganda","")
-        var contact2=Contact("speria","prudeahimbisibwe@gmail.com","0784027930","uganda","")
-        var contact3=Contact("Ashly","prudeahimbisibwe@gmail.com","0784027930","uganda","")
-        var contact4=Contact("Annena","prudeahimbisibwe@gmail.com","0784027930","uganda","")
+        var contact=Contact("Prudence","prudeahimbisibwe@gmail.com","0784027930","uganda","https://media.istockphoto.com/photos/beautiful-black-woman-picture-id497864320?b=1&k=20&m=497864320&s=170667a&w=0&h=jPVYG0ckQ9ObWvY7GT7MVZvn7GHOJ7hivIP3e_W0tYc=")
+        var contact1=Contact("dallen","prudeahimbisibwe@gmail.com","0784027930","uganda","https://media.istockphoto.com/photos/beautiful-black-woman-picture-id497864320?b=1&k=20&m=497864320&s=170667a&w=0&h=jPVYG0ckQ9ObWvY7GT7MVZvn7GHOJ7hivIP3e_W0tYc=")
+        var contact2=Contact("speria","prudeahimbisibwe@gmail.com","0784027930","uganda","https://media.istockphoto.com/photos/beautiful-black-woman-picture-id497864320?b=1&k=20&m=497864320&s=170667a&w=0&h=jPVYG0ckQ9ObWvY7GT7MVZvn7GHOJ7hivIP3e_W0tYc=")
+        var contact3=Contact("Ashly","prudeahimbisibwe@gmail.com","0784027930","uganda","https://media.istockphoto.com/photos/beautiful-black-woman-picture-id497864320?b=1&k=20&m=497864320&s=170667a&w=0&h=jPVYG0ckQ9ObWvY7GT7MVZvn7GHOJ7hivIP3e_W0tYc=")
+        var contact4=Contact("Annena","prudeahimbisibwe@gmail.com","0784027930","uganda","https://media.istockphoto.com/photos/beautiful-black-woman-picture-id497864320?b=1&k=20&m=497864320&s=170667a&w=0&h=jPVYG0ckQ9ObWvY7GT7MVZvn7GHOJ7hivIP3e_W0tYc=")
         var contactList= listOf(contact,contact1,contact2,contact3,contact4)
 
         var contactRvAdapter=ContactRvAdapter(contactList)
